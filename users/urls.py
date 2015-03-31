3403a0e67f4ba6341977819988b95c449e2f3cac
Jim Grant <jimgrant@gmail.com>
2015-03-31 16:02:04 -0700

    add new user feature

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,7 @@ from users import views
 urlpatterns = patterns('',
         url(r'^$', views.index, name='index'),
         url(r'^(?P<user_id>\d+)/$', views.detail, name='detail'),
+        url(r'^/add/$', views.add, name='add'),
         url(r'^(?P<user_id>\d+)/edit/$', views.edit, name='edit'),
+        url(r'^(?P<user_id>\d+)/delete/$', views.edit, name='delete'),
 )
--- users/views.py
+++ users/views.py
@@ -8,15 +8,37 @@ def index(request):
     context = {'user_list': User.objects.order_by('first_name')}
     return render(request, 'users/index.html', context)
 
-def detail(request, user_id):
+def detail(request, user_id=None):
     try:
         user = User.objects.get(pk=user_id)
     except User.DoesNotExist:
         raise Http404("User does not exist")
+
+    return render(request, 'users/detail.html', {'user': user})
+
+def add(request):
+    user = {
+        'id': 0,
+        'first_name': '',
+        'last_name': '',
+        'email': ''
+    }
+
     return render(request, 'users/detail.html', {'user': user})
 
 def edit(request, user_id):
-    p = get_object_or_404(User, pk=user_id)
+    if user_id != '0':
+        p = get_object_or_404(User, pk=user_id)
+    else:
+        # user id of 0 means add a new user
+        p = User()
+
     p.first_name = request.POST['first_name']
+    p.last_name = request.POST['last_name']
+    p.email = request.POST['email']
+
     p.save()
+    return HttpResponseRedirect(reverse('index'))
+
+def delete(request):
     return HttpResponseRedirect(reverse('index'))
