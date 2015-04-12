e39658dd8ddd2a1bb824e1643484e74f28b08aac
Jim Grant <jimgrant@gmail.com>
2015-04-12 14:04:31 -0700

    perform redirect on base url

--- cf_exam_django/urls.py
+++ cf_exam_django/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 urlpatterns = patterns('',
-    url(r'^$', include('users.urls')),
+    url(r'^$', RedirectView.as_view(url='users', permanent=False)),
     url(r'^users/', include('users.urls')),
     url(r'^admin/', include(admin.site.urls))
 )
