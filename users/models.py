0a0bb664b3a6a7273f739ce2a52f5f5c06b7288a
Jim Grant <jimgrant@gmail.com>
2015-04-13 21:27:11 -0700

    names and bios abbreviated on index

--- users/models.py
+++ users/models.py
@@ -7,5 +7,17 @@ class User(models.Model):
     email = models.EmailField(max_length=254)
     bio = models.TextField(default="")
 
+    @property
+    def name_abbrev(self):
+        name = str(self)
+        return name[:25] + "..." if len(name) > 28 else name
+
+    @property
+    def bio_abbrev(self):
+        if self.bio:
+            return self.bio[:30] + "..." if len(self.bio) > 33 else self.bio
+        else:
+            return "..."
+
     def __str__(self):
         return " ".join([self.first_name, self.last_name])
