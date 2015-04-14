31476f1b138dc38ffb4bebbd495120f1acac55ec
Jim Grant <jimgrant@gmail.com>
2015-04-14 14:01:28 -0700

    cleanup of files, css modifications

--- users/static/users/js/deleteConfirm.js
+++ users/static/users/js/deleteConfirm.js
@@ -4,8 +4,6 @@ $('#deleteConfirm').on('show.bs.modal', function (event) {
     // Extract info from data-* attributes
     var href = button.data('href');
     var name = button.data('name');
-    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
     var modal = $(this);
     modal.find('.delete-warning').text('Are you sure you want to delete ' + name + '?');
     modal.find('.delete-button').attr('href', href);
